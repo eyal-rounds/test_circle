a5b3a3438960a09d620934c0d9e867c97fc2bb12
eyal <eyal@rounds.com>
2015-12-09 15:19:34 +0200

    movved scripts dir encapsulated pre actions in dependencies into one script

---
+++ scripts/make_build_count.sh
@@ -0,0 +1,27 @@
+#!/usr/bin/bash
+
+APP_DIR=app
+
+#build the chache dir
+if [[ ! -d ~/.rounds_cache/ ]]
+    then mkdir -p ~/.rounds_cache/
+fi
+#get current version name from the gradle file.
+curr_ver=$(cat ${APP_DIR}/build.gradle |grep versionName|tr -d ' \t'|sed -e 's/ *versionName *\"\(.*\)\"/\1/g')
+if [[ ! -f ~/.rounds_cache/version_name ]]
+    then echo $curr_ver > ~/.rounds_cache/version_name
+fi
+#get the cached version name
+cached_ver=$(cat ~/.rounds_cache/version_name|tr -d ' \t')
+# if no build count file OR new app version name restart the count.
+if [[ "$ver" != "$cached_ver" ]] || [[ ! -f ~/.rounds_cache/build_count ]]
+    echo \0 > ~/.rounds_cache/build_count
+else
+    #if we already have build count file, increase it by one for the build process.
+    count=$(cat ~/.rounds_cache/build_count|tr -d ' \t')
+    echo $((count+1))> ~/.rounds_cache/build_count
+fi
+#if it's just new version also cache the new version name
+if [[ "$ver" != "$cached_ver" ]]
+    then echo $curr_ver > ~/.rounds_cache/version_name
+fi
