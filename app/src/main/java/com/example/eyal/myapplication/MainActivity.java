8242ad289c2fc15a0e63cef4141e1da33d8d154f
eyal <eyal@rounds.com>
2016-09-13 23:04:50 +0300

    adding native jni code  tests
    building using the new cmake plugin

--- app/src/main/java/com/example/eyal/myapplication/MainActivity.java
+++ app/src/main/java/com/example/eyal/myapplication/MainActivity.java
@@ -23,6 +23,7 @@ public class MainActivity extends AppCompatActivity {
 		} catch (PackageManager.NameNotFoundException e) {
 			e.printStackTrace();
 		}
+        Log.e("XXX", stringFromJNI());
         setContentView(R.layout.activity_main);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
@@ -57,5 +58,28 @@ public class MainActivity extends AppCompatActivity {
         }
 
         return super.onOptionsItemSelected(item);
+    }
+
+    public native String  stringFromJNI();
+
+    /* This is another native method declaration that is *not*
+     * implemented by 'hello-jni'. This is simply to show that
+     * you can declare as many native methods in your Java code
+     * as you want, their implementation is searched in the
+     * currently loaded native libraries only the first time
+     * you call them.
+     *
+     * Trying to call this function will result in a
+     * java.lang.UnsatisfiedLinkError exception !
+     */
+    public native String  unimplementedStringFromJNI();
+
+    /* this is used to load the 'hello-jni' library on application
+     * startup. The library has already been unpacked into
+     * /data/data/com.example.hellojni/lib/libhello-jni.so at
+     * installation time by the package manager.
+     */
+    static {
+        System.loadLibrary("hello-jni");
     }
 }
